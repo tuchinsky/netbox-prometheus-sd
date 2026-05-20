759eda8a961cfde5b1896cb0dc4346d23e922fea
Artem Tuchinsky <tuchinsky@gmail.com>
2026-05-20 11:24:00 +0400

    add missing fields to plugin __init__.py

--- netbox_prometheus_http_sd/__init__.py
+++ netbox_prometheus_http_sd/__init__.py
@@ -9,6 +9,8 @@ class NetBoxPrometheusSDConfig(PluginConfig):
     name = 'netbox_prometheus_http_sd'
     verbose_name = 'NetBox Prometheus HTTP SD'
     description = 'A Netbox plugin to export targets for Prometheus HTTP service discovery'
+    author = 'Artem Tuchinsky'
+    author_email = 'tuchinsky@gmail.com'
     version = __version__
     base_url = 'prometheus-http-sd'
 
